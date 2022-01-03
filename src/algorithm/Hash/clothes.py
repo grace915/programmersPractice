6c3ecd498d81b8bdac92ed853908db15862380fa
grace915 <grace0807@hanyang.ac.kr>
2022-01-04 01:22:41 +0900

    1.3 clothes, phone

---
+++ src/algorithm/Hash/clothes.py
@@ -0,0 +1,10 @@
+import collections
+
+def solution(clothes):
+    answer = 0
+    return answer
+
+
+clothes = [["1", "face"], ["2", "face"], ["3", "top"], ["4", "pants"], ["5", "outer"]]
+# 한 종류 하나만, 옷은 하나이상
+# 부산 바다, 서면, 해운대
