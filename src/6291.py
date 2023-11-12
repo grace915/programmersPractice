e11cd3643ed3002ec0071011782c91c029bdf998
grace915 <grace0807@hanyang.ac.kr>
2023-11-12 23:46:45 +0900

    softeer 6291 수정

--- src/6291.py
+++ src/6291.py
@@ -3,9 +3,9 @@ input = sys.stdin.readline
 
 N = int(input())
 classes = [list(map(int, input().split())) for _ in range(N)]
-cc = sorted(classes, key = lambda x : [x[1], x[0]])
+classes.sort(key=lambda x:x[1])
 # print(classes)
-answer = [cc[0]]
+answer = [classes[0]]
 for i in range(1, len(classes)):
   start, end = classes[i]
   if start >= answer[-1][1]:
